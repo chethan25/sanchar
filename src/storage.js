d0d51d48922bff4b931d61a55317824eda1028e1
Chethan B <chethan25.bpac@gmail.com>
2021-10-06 12:01:06 +0530

    initialize firebase app and added uploadFile func to upload files to storage

---
+++ src/storage.js
@@ -0,0 +1,36 @@
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  deleteObject,
+} from 'firebase/storage';
+import { getFirebaseConfig } from './firebase-config';
+import { initializeApp } from 'firebase/app';
+import to from 'await-to-js';
+
+const firebaseAppConfig = getFirebaseConfig();
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseAppConfig);
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+const storage = getStorage(firebaseApp);
+
+// Upload the file to firebase cloud storage
+export async function uploadFile(file, uniqueId) {
+  try {
+    // Create a storage reference from our storage service
+    const storageRef = ref(storage, `${uniqueId}/${file.name}`);
+    const fileSnapshot = await uploadBytesResumable(storageRef, file);
+    // Generate a public URL for the file
+    const url = await getDownloadURL(storageRef);
+
+    return url;
+  } catch (error) {
+    console.error(
+      'There was an error uploading a file to Cloud Storage:',
+      error
+    );
+  }
+}
